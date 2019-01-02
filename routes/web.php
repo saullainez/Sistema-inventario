8f1a4f7cb9d56f44d8fc0b0fb4a28abf7926f3dd
jos0409007 <josuegt_07@hotmail.com>
2019-01-02 13:30:09 -0600

    cambios en rutas

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,21 @@ Route::get('/registro', function(){
     return view('formulario');
 });
 Route::resource('tipo-bebida', 'TipoBebidaController');
+Route::resource('presentacion', 'PresentacionController');
+Route::resource('producto', 'ProductoController');
+Route::resource('empresa', 'EmpresaController');
+Route::resource('conceptos', 'MovimientoConceptoController');
+Route::resource('movimiento-producto', 'MovimientoProductoController');
+Route::resource('movimiento-activo', 'MovimientoActivoController');
+Route::resource('activo', 'ActivoController');
+Route::resource('inventario', 'InventarioController');
+Route::resource('inventario-activo', 'InventarioActivoController');
+
+Route::get('/proveedor','EmpresaController@proveedores');
+Route::get('clientes','EmpresaController@clientes');
+Route::get('/entradas','MovimientoConceptoController@entradas');
+Route::get('/salidas','MovimientoConceptoController@salidas');
 
 Route::get('/identificacion', function(){
     return csrf_token();
 });
-
-Route::resource('presentacion', 'PresentacionController');
-Route::resource('producto', 'ProductoController');
-Route::resource('empresa', 'EmpresaController');
-Route::get('/proveedor','EmpresaController@proveedores');
-Route::get('clientes','EmpresaController@clientes');
-Route::resource('movimientos', 'MovimientoConceptoController');
-Route::get('/entradas','MovimientoConceptoController@entradas');
-Route::get('/salidas','MovimientoConceptoController@salidas');
