dab84f2cc3676c2e5535245e6bdd2637a0d697bc
Saul Lainez <saullainez@hotmail.es>
2019-01-03 20:10:26 -0600

    Actualizacion de la vista principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,105 +78,63 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a style="color:white; font-size: 1.4rem;" href="{{ url('/home') }}">Inicio</a>
+                        <a style="color:white; font-size: 1.4rem;" href="{{ url('/home') }}">Dashboard</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
-                <!--<div class="title m-b-md white-text">
-                    Laravel
-                </div>
+                @guest
+                    <div class="contenedor-formulario-registro">
+                        <h1 style="margin-bottom:2rem;">Iniciar sesión</h1>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
 
-                <div class="links">
-                    <a class="white-text" href="https://laravel.com/docs">Documentation</a>
-                    <a class="white-text" href="https://laracasts.com">Laracasts</a>
-                    <a class="white-text" href="https://laravel-news.com">News</a>
-                    <a class="white-text" href="https://nova.laravel.com">Nova</a>
-                    <a class="white-text" href="https://forge.laravel.com">Forge</a>
-                    <a class="white-text" href="https://github.com/laravel/laravel">GitHub</a>
-                </div>-->
-                <div class="contenedor-formulario-registro">
-                    <h1 style="margin-bottom:2rem;">Iniciar sesión</h1>
-                    <!--<form class="form-horizontal" method="POST" action="{{ url('login') }}">
-                        {{ csrf_field() }}
-        
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="control-label">Correo electrónico</label>
-        
-                            <div>
-                                <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-        
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                            <div class="form-group">
+                                <label for="email" class="control-label">{{ __('Correo electrónico') }}</label>
+
+                                <div>
+                                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group{{ $errors->has('pass') ? ' has-error' : '' }}">
-                            <label for="pass" class="control-label">Contraseña</label>
-        
-                            <div>
-                                <input id="pass" type="text" class="form-control" name="pass" value="{{ old('pass') }}" required autofocus>
-        
-                                @if ($errors->has('pass'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('pass') }}</strong>
-                                    </span>
-                                @endif
+
+                            <div class="form-group">
+                                <label for="password" class="">{{ __('Password') }}</label>
+
+                                <div>
+                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
-                        </div>         
-                        <div class="form-group">
-                            <div>
-                                <button type="submit" class="btn btn-primary">
-                                    Iniciar sesión
-                                </button>
+
+
+
+                            <div class="form-group">
+                                <div>
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Iniciar sesión') }}
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>-->
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group">
-                            <label for="email" class="control-label">{{ __('Correo electrónico') }}</label>
-
-                            <div>
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password" class="">{{ __('Password') }}</label>
-
-                            <div>
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-
-
-                        <div class="form-group">
-                            <div>
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Iniciar sesión') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        </form>
+                    </div>
+                @else
+                    <div class="contenedor-formulario-registro">
+                        <h1>Bienvenido</h1>
+                        <a style="color:white; font-size: 1.4rem;" href="{{ url('/home') }}">Dashboard</a>
+                    </div>
+                @endguest
 
             </div>
         </div>
