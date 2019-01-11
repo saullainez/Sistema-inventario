76e9e27827bd74b701b05c78694c89d710c37325
Saul Lainez <saullainez@hotmail.es>
2019-01-10 19:03:40 -0600

    Actualización de Crear archivo usando el PA

--- app/Http/Controllers/ActivoController.php
+++ app/Http/Controllers/ActivoController.php
@@ -64,13 +64,13 @@ class ActivoController extends Controller
             $activo->ActivoNombre = $request->Input('ActivoNombre');
             $activo->ActivoDescripcion = $request->Input('ActivoDescripcion');
             $activo->TipoActivo = $request->Input('TipoActivo');
-            $activo->save();
-            return response()->json([
+            //$activo->save();
+            /*return response()->json([
                 "mensaje" => "Activo creado correctamente"
-            ]);
+            ]);*/
 
-            /*$res = Activo::crearActivo($activo);
-            return response()->json($res, 200)->header('Content-Type','application/json');*/
+            $res = Activo::crearActivo($activo);
+            return response()->json([$res, "mensaje" =>"Activo creado correctamente"], 200)->header('Content-Type','application/json');
         }
         catch(\Exception $e){
             $error = ['error' => $e->getMessage()];
