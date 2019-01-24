55c444fde8d30306b9d5dd94d95d7de5d3c1ddcd
Saul Lainez <saullainez@hotmail.es>
2019-01-24 17:38:52 -0600

    Actualización del dashboard con charts nuevos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,10 +62,13 @@
                     <i class="far fa-check-circle"></i>
                 </span>
                 <div class="caja-info-contenido">
-                    <span class="caja-info-texto">Activos</span>
+                    <span class="caja-info-texto">Materia prima</span>
                     <span class="caja-info-numero">{{ count($activos) }}</span>
-                    <div class="caja-divisor"></div>
-                    <span class="caja-administrar"><a href="/activo" class="boton">Administrar</a></span>
+                    <span class="caja-info-desc">Consumibles: {{ count($consumibles) }}</span>
+                    <span class="caja-info-desc">Equipo: {{ count($equipos) }}</span>
+
+                    <div class="caja-divisor-esp"></div>
+                    <span class="caja-administrar-esp"><a href="/activo" class="boton">Administrar</a></span>
                 </div>
             </div>
         </div>
@@ -110,83 +113,144 @@
                                 <i class="fas fa-warehouse"></i>
                             </span>
                             <div class="caja-info-contenido">
-                                <span class="caja-info-texto" style="text-align:center;">Inventario de activos</span>
-                                <span class="caja-info-numero" style="text-align:center;">{{ count($inventariosActivos) }}</span>
-                                <div class="caja-divisor"></div>
-                                <span class="caja-administrar"><a href="/reporte-inventario" target="_blank" class="boton"><i class="fas fa-eye mt-0 mr-1"></i>Ver reporte</a></span>
+                                <span class="caja-info-texto-chart">Inventario de materia prima</span>
+                                <span class="caja-info-numero-chart">{{ count($activos) }}</span>
+                                <span class="caja-info-chart">Consumibles: {{ count($consumibles) }}</span>
+                                <span class="caja-info-chart">Equipo: {{ count($equipos) }}</span>
+                                <div class="caja-divisor-chart"></div>
+                                <span class="caja-administrar-esp"><a href="/reporte-inventario" target="_blank" class="boton"><i class="fas fa-eye mt-0 mr-1"></i>Ver reporte</a></span>
                             </div>
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-12">
-                        <canvas id="myChart"></canvas>
+                    <div class="col-12 col-sm-12 col-md-7 col-lg-7">
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="consumibles-tab" data-toggle="tab" href="#consumibles" role="tab" aria-controls="consumibles" aria-selected="true">Consumibles</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="equipos-tab" data-toggle="tab" href="#equipos" role="tab" aria-controls="equipos" aria-selected="false">Equipos</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="consumibles" role="tabpanel" aria-labelledby="consumibles-tab">
+                                <canvas id="chartConsumibles"></canvas>
+                            </div>
+                            <div class="tab-pane fade" id="equipos" role="tabpanel" aria-labelledby="equipos-tab">
+                                <canvas id="chartEquipos"></canvas>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-5 col-lg-5">
+                        <canvas id="tipoMP"></canvas>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!--<div class="col-md-9 mb-4">
-
-        <div class="card">
-            <div class="card-header text-center">
-                Pie chart
-            </div>
-
-            <div class="card-body">
-
-                <canvas id="myChart"></canvas>
-
-            </div>
-
-        </div>
-    </div>-->
     
 </div>
 @endsection
 @section('scripts')
 <script type="text/javascript" src="js/home.js"></script>
 <script>
-    var inventarioActivos = @json($inventariosActivos);
-    var labels=[];
-    var data=[];
-    for (i = 0; i < inventarioActivos.length; i++){
-        labels.push(inventarioActivos[i].ActivoNombre);
-        data.push(inventarioActivos[i].Cantidad);
+    var consumibles = @json($consumibles);
+    var equipos = @json($equipos);
+    var labelsConsumibles=[];
+    var dataConsumibles=[];
+    var labelsEquipos=[];
+    var dataEquipos=[];
+    var coloresConsumibles=[];
+    var coloresEquipos=[];
+    var coloresDinamicos = function() {
+            var r = Math.floor(Math.random() * 255);
+            var g = Math.floor(Math.random() * 255);
+            var b = Math.floor(Math.random() * 255);
+            return "rgb(" + r + "," + g + "," + b + ")";
+         };
+    for (i = 0; i < consumibles.length; i++){
+        labelsConsumibles.push(consumibles[i].ActivoNombre);
+        dataConsumibles.push(consumibles[i].Cantidad);
+        coloresConsumibles.push(coloresDinamicos());
     }
-    var ctx = document.getElementById("myChart").getContext('2d');
+    for (i = 0; i < equipos.length; i++){
+        labelsEquipos.push(equipos[i].ActivoNombre);
+        dataEquipos.push(equipos[i].Cantidad);
+        coloresEquipos.push(coloresDinamicos());
+    }
+    var ctx = document.getElementById("chartConsumibles").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
 
-            labels: labels ,
+            labels: labelsConsumibles,
             datasets: [{
-                label: "Inventario de activos",
-                data: data,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                label: "Consumibles",
+                data: dataConsumibles,
+                backgroundColor: coloresConsumibles,
                 borderWidth: 1
-            }]
+                }]
         },
         options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Consumibles'
+            },
             scales: {
                 yAxes: [{
                     ticks: {
                         beginAtZero: true
                     }
                 }]
+            }
+        }
+    });
+    var eqp = document.getElementById("chartEquipos").getContext('2d');
+    var chartEquipos = new Chart(eqp, {
+        type: 'bar',
+        data: {
+
+            labels: labelsEquipos,
+            datasets: [{
+                label: "Equipos",
+                data: dataEquipos,
+                backgroundColor: coloresEquipos,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Equipos'
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    var tipo = document.getElementById("tipoMP").getContext('2d');
+    var chartTipoMP = new Chart(tipo, {
+        type: 'pie',
+        data: {
+            labels: ["Consumibles", "Equipos"],
+            datasets: [{
+                label: "Tipo de materia prima",
+                data: [consumibles.length, equipos.length],
+                //data: [3, 2],
+                backgroundColor: ["#3e95cd", "#c45850"],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            //legend: { display: false },
+            title: {
+                display: true,
+                text: 'Tipo de materia prima'
             }
         }
     });
